60f4ade900f34df74254978094c680c38d38bc7e
Daniel <dterrero10@gmail.com>
2024-10-06 17:23:55 -0400

    try2

--- project-managemet/screens/StepScreen.js
+++ project-managemet/screens/StepScreen.js
@@ -7,230 +7,301 @@ import { useEffect } from 'react';
 import { Image } from 'react-native';
 import backIcon from '../assets/return-apple-icon.png';
 import greenIcon from '../assets/camera-green-icon.png';
-import redIcon from '../assets/camera-red-icon.png' 
+import redIcon from '../assets/camera-red-icon.png'
 
 const StepScreen = ({ route }) => {
-  const { stepId, projectName, email, projectId } = route.params;
-  const [StepDetails, setStepDetails] = useState({});
-  const [StepDate, setStepDate] = useState();
-  const navigation = useNavigation();
-  const [formData, setFormData] = useState({
-    is_done: false, 
-    description: "",
-    file: null,
-    file2: null,
-    file3: null,
-    file4: null,
-    sign_sheet: null,
-  });
+    const { stepId, projectName, email, projectId } = route.params;
+    const [StepDetails, setStepDetails] = useState({});
+    const [StepDate, setStepDate] = useState();
+    const navigation = useNavigation();
+    const [formData, setFormData] = useState({
+        is_done: false,
+        description: "",
+        file: null,
+        file2: null,
+        file3: null,
+        file4: null,
+        sign_sheet: null,
+    });
 
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      // Fetch step details
-      const stepResponse = await axios.get(`http://10.55.6.135:8000/api/get_step_details/${stepId}/`);
-      setStepDetails(stepResponse.data);
-      const step  = stepResponse.data;
-      const updatedFormData = {
-        is_done: step.is_done,
-        description: step.description,
-        file: step.file || null,
-        file2: step.file2 || null,
-        file3: step.file3 || null,
-        file4: step.file4 || null,
-        sign_sheet: step.sign_sheet || null,
-      };
-      setStepDate(step.todays_date);
-      
-      setFormData(updatedFormData);
+    useEffect(() => {
+        const fetchData = async() => {
+            try {
+                // Fetch step details
+                const stepResponse = await axios.get(`http://10.55.6.135:8000/api/get_step_details/${stepId}/`);
+                setStepDetails(stepResponse.data);
+                const step = stepResponse.data;
+                const updatedFormData = {
+                    is_done: step.is_done,
+                    description: step.description,
+                    file: step.file || null,
+                    file2: step.file2 || null,
+                    file3: step.file3 || null,
+                    file4: step.file4 || null,
+                    sign_sheet: step.sign_sheet || null,
+                };
+                setStepDate(step.todays_date);
 
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
+                setFormData(updatedFormData);
 
-  fetchData();
-}, [stepId]); // Dependency array includes stepId// Empty dependency array for camera permissions
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, [stepId]); // Dependency array includes stepId// Empty dependency array for camera permissions
 
 
 
-  const backToHome = () => {
-    console.log("Navigate")
-    navigation.navigate('Home');
-  };
-  const openImagePicker = async (fieldName) => {
-    try {
-      const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
-  
-      if (permissionResult.granted === false) {
-        alert("You've refused to allow this app to access your camera!");
-        return;
-      }
-  
-      const result = await ImagePicker.launchCameraAsync({
-        base64: true,
-        quality: 1,
-      });  
-      if (!result.cancelled) {
-        // Update the formData with the base64 data directly
-        setFormData({ ...formData, [fieldName]: result.assets[0].base64 });
-      }
-    } catch (error) {
-      console.error('Error picking image:', error);
-    }
-  };
+    const backToHome = () => {
+        console.log("Navigate")
+        navigation.navigate('Home');
+    };
+    const openImagePicker = async(fieldName) => {
+        try {
+            const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
 
-  const handleEditStep = async () => {
-    navigation.navigate('Home');
-    try {
-      const dataToSend = {
-        is_done: formData.is_done.toString(),
-        description: formData.description,
-      };
+            if (permissionResult.granted === false) {
+                alert("You've refused to allow this app to access your camera!");
+                return;
+            }
 
-      // Append base64 fields only if they are not null
-      Object.keys(formData).forEach((fieldName) => {
-        if (fieldName.startsWith('file') && formData[fieldName] || fieldName.startsWith('sign_sheet') && formData[fieldName] ) {
-          dataToSend[fieldName] = formData[fieldName];
+            const result = await ImagePicker.launchCameraAsync({
+                base64: true,
+                quality: 1,
+            });
+            if (!result.cancelled) {
+                // Update the formData with the base64 data directly
+                setFormData({...formData, [fieldName]: result.assets[0].base64 });
+            }
+        } catch (error) {
+            console.error('Error picking image:', error);
         }
-      });
+    };
 
-      const response = await axios.post(`http://10.55.6.135:8000/api/edit-step/${stepId}/`, dataToSend, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+    const handleEditStep = async() => {
+        navigation.navigate('Home');
+        try {
+            const dataToSend = {
+                is_done: formData.is_done.toString(),
+                description: formData.description,
+            };
 
-      console.log('Step updated successfully:', response.data);
-    } catch (error) {
-      console.error('Error updating step:', error);
-    }
-  };
+            // Append base64 fields only if they are not null
+            Object.keys(formData).forEach((fieldName) => {
+                if (fieldName.startsWith('file') && formData[fieldName] || fieldName.startsWith('sign_sheet') && formData[fieldName]) {
+                    dataToSend[fieldName] = formData[fieldName];
+                }
+            });
 
-  return (
-<View style={styles.container}>
-  <TouchableOpacity style={styles.logoutButton} onPress={backToHome}>
-    <Image source={backIcon} style={{ width: 20, height: 20 }} />
-  </TouchableOpacity>
-  <Text style={styles.welcomeText}>Assignment</Text>
+            const response = await axios.post(`http://10.55.6.135:8000/api/edit-step/${stepId}/`, dataToSend, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
 
-    <View style={styles.projectContainer}>
-    <Text style={{ fontSize: 18, marginBottom: 20, fontWeight: 'bold' }}>{projectName}</Text>
-     <Text style={{ fontSize: 18, marginBottom: 20}}><Text style={{ fontWeight: 'bold'  }}>Date: </Text>{StepDate}</Text>
+            console.log('Step updated successfully:', response.data);
+        } catch (error) {
+            console.error('Error updating step:', error);
+        }
+    };
 
-    <Text style={{ fontSize: 18, marginBottom: 5, fontWeight: 'bold' }}>Assignment Finished</Text>
-    <Switch style={{marginBottom: 10}} value={formData.is_done} onValueChange={(value) => setFormData({ ...formData, is_done: value })} />
+    return ( <
+        View style = { styles.containervv } >
+        <
+        TouchableOpacity style = { styles.logoutButton }
+        onPress = { backToHome } >
+        <
+        Image source = { backIcon }
+        style = {
+            { width: 20, height: 20 } }
+        /> <
+        /TouchableOpacity> <
+        Text style = { styles.welcomeText } > Assignment < /Text>
 
-    <Text style={{ fontSize: 18, marginBottom: 5, fontWeight: 'bold' }} >Additional Comments:</Text>
-    <TextInput style={{ fontSize: 18, marginBottom: 20 }} 
-      value={formData.description}
-      onChangeText={(text) => setFormData({ ...formData, description: text })}
-      placeholder="Enter description"
-    />
+        <
+        View style = { styles.projectContainer } >
+        <
+        Text style = {
+            { fontSize: 18, marginBottom: 20, fontWeight: 'bold' } } > { projectName } < /Text> <
+        Text style = {
+            { fontSize: 18, marginBottom: 20 } } > < Text style = {
+            { fontWeight: 'bold' } } > Date: < /Text>{StepDate}</Text >
 
-<View style={styles.imageContainer}>
-  <TouchableOpacity onPress={() => openImagePicker('file')}>
-    {formData.file ? (
-      <Image source={greenIcon} style={{ width: 30, height: 30, marginRight: 10 }} />
-    ) : (
-      <Image source={redIcon} style={{ width: 30, height: 30, marginRight: 10 }} />
-    )}
-    <Text>9am</Text>
-  </TouchableOpacity>
+        <
+        Text style = {
+            { fontSize: 18, marginBottom: 5, fontWeight: 'bold' } } > Assignment Finished < /Text> <
+        Switch style = {
+            { marginBottom: 10 } }
+        value = { formData.is_done }
+        onValueChange = {
+            (value) => setFormData({...formData, is_done: value }) }
+        />
 
-  <TouchableOpacity onPress={() => openImagePicker('file2')}>
-    {formData.file2 ? (
-      <Image source={greenIcon} style={{ width: 30, height: 30, marginRight: 10 }} />
-    ) : (
-      <Image source={redIcon} style={{ width: 30, height: 30, marginRight: 10 }} />
-    )}
-    <Text>11am</Text>
-  </TouchableOpacity>
+        <
+        Text style = {
+            { fontSize: 18, marginBottom: 5, fontWeight: 'bold' } } > Additional Comments: < /Text> <
+        TextInput style = {
+            { fontSize: 18, marginBottom: 20 } }
+        value = { formData.description }
+        onChangeText = {
+            (text) => setFormData({...formData, description: text }) }
+        placeholder = "Enter description" /
+        >
 
-  <TouchableOpacity onPress={() => openImagePicker('file3')}>
-    {formData.file3 ? (
-      <Image source={greenIcon} style={{ width: 30, height: 30, marginRight: 10 }} />
-    ) : (
-      <Image source={redIcon} style={{ width: 30, height: 30, marginRight: 10 }} />
-    )}
-    <Text>1pm</Text>
-  </TouchableOpacity>
+        <
+        View style = { styles.imageContainer } >
+        <
+        TouchableOpacity onPress = {
+            () => openImagePicker('file') } > {
+            formData.file ? ( <
+                Image source = { greenIcon }
+                style = {
+                    { width: 30, height: 30, marginRight: 10 } }
+                />
+            ) : ( <
+                Image source = { redIcon }
+                style = {
+                    { width: 30, height: 30, marginRight: 10 } }
+                />
+            )
+        } <
+        Text > 9 am < /Text> <
+        /TouchableOpacity>
 
-  <TouchableOpacity onPress={() => openImagePicker('file4')}>
-    {formData.file4 ? (
-      <Image source={greenIcon} style={{ width: 30, height: 30, marginRight: 10 }} />
-    ) : (
-      <Image source={redIcon} style={{ width: 30, height: 30, marginRight: 10 }} />
-    )}
-    <Text>4pm</Text>
-  </TouchableOpacity>
+        <
+        TouchableOpacity onPress = {
+            () => openImagePicker('file2') } > {
+            formData.file2 ? ( <
+                Image source = { greenIcon }
+                style = {
+                    { width: 30, height: 30, marginRight: 10 } }
+                />
+            ) : ( <
+                Image source = { redIcon }
+                style = {
+                    { width: 30, height: 30, marginRight: 10 } }
+                />
+            )
+        } <
+        Text > 11 am < /Text> <
+        /TouchableOpacity>
 
-  <TouchableOpacity onPress={() => openImagePicker('sign_sheet')}>
-    {formData.sign_sheet ? (
-      <Image source={greenIcon} style={{ width: 30, height: 30 }} />
-    ) : (
-      <Image source={redIcon} style={{ width: 30, height: 30 }} />
-    )}
-    <Text>sheet</Text>
-  </TouchableOpacity>
-</View>
+        <
+        TouchableOpacity onPress = {
+            () => openImagePicker('file3') } > {
+            formData.file3 ? ( <
+                Image source = { greenIcon }
+                style = {
+                    { width: 30, height: 30, marginRight: 10 } }
+                />
+            ) : ( <
+                Image source = { redIcon }
+                style = {
+                    { width: 30, height: 30, marginRight: 10 } }
+                />
+            )
+        } <
+        Text > 1 pm < /Text> <
+        /TouchableOpacity>
 
-    {/* Edit Step button to submit the form */}
-  </View>
-      <TouchableOpacity onPress={handleEditStep}>
-        <Text style={{ fontSize: 18, marginBottom: 20, fontWeight: 'bold', color: '#206422' }}>Save</Text>
-      </TouchableOpacity>
-  
-</View>
-  );
+        <
+        TouchableOpacity onPress = {
+            () => openImagePicker('file4') } > {
+            formData.file4 ? ( <
+                Image source = { greenIcon }
+                style = {
+                    { width: 30, height: 30, marginRight: 10 } }
+                />
+            ) : ( <
+                Image source = { redIcon }
+                style = {
+                    { width: 30, height: 30, marginRight: 10 } }
+                />
+            )
+        } <
+        Text > 4 pm < /Text> <
+        /TouchableOpacity>
+
+        <
+        TouchableOpacity onPress = {
+            () => openImagePicker('sign_sheet') } > {
+            formData.sign_sheet ? ( <
+                Image source = { greenIcon }
+                style = {
+                    { width: 30, height: 30 } }
+                />
+            ) : ( <
+                Image source = { redIcon }
+                style = {
+                    { width: 30, height: 30 } }
+                />
+            )
+        } <
+        Text > sheet < /Text> <
+        /TouchableOpacity> <
+        /View>
+
+        { /* Edit Step button to submit the form */ } <
+        /View> <
+        TouchableOpacity onPress = { handleEditStep } >
+        <
+        Text style = {
+            { fontSize: 18, marginBottom: 20, fontWeight: 'bold', color: '#206422' } } > Save < /Text> <
+        /TouchableOpacity>
+
+        <
+        /View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F0F0F0',
-    paddingTop: 80,
-    alignItems: 'center', // Center content horizontally within the screen
-  },
-  welcomeText: {
-    fontSize: 25,
-    marginBottom: 10,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  scrollContainer: {
-    alignItems: 'center', // Center content horizontally within the ScrollView
-  },
-  projectContainer: {
-    backgroundColor: '#FFFFFF',
-    padding: 20,
-    borderRadius: 20,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
+    container: {
+        flex: 1,
+        backgroundColor: '#F0F0F0',
+        paddingTop: 80,
+        alignItems: 'center', // Center content horizontally within the screen
     },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-    marginTop: 20,
-    marginBottom: 20,  // Add marginRight to create space after the pictures
-  },
-  logoutButton: {
-    position: 'absolute',
-    top: 50,
-    right: 10,
-  },
-  logoutText: {
-    color: '#206422',
-    fontSize: 16,
-  },
+    welcomeText: {
+        fontSize: 25,
+        marginBottom: 10,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    scrollContainer: {
+        alignItems: 'center', // Center content horizontally within the ScrollView
+    },
+    projectContainer: {
+        backgroundColor: '#FFFFFF',
+        padding: 20,
+        borderRadius: 20,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        marginTop: 20,
+        marginBottom: 20, // Add marginRight to create space after the pictures
+    },
+    logoutButton: {
+        position: 'absolute',
+        top: 50,
+        right: 10,
+    },
+    logoutText: {
+        color: '#206422',
+        fontSize: 16,
+    },
 
-  imageContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    imageContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 
 });
 export default StepScreen;
